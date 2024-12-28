53b8893dd456b090fac533dfe3d456cd45bfa6d8
nike-dev <146763214+nike-dev@users.noreply.github.com>
2024-12-28 13:51:26 +0200

    Create playGame function to play 5 rounds
    Used for loop to play 4 consecutive rounds. Then,
    created last instance of a round to tally and
    declare final winner.

--- script.js
+++ script.js
@@ -18,55 +18,74 @@ function getComputerChoice() {
 //  return choice;}
 
 function getHumanChoice() {
-    let choice = prompt("What do you choose?", "Rock or Paper or Scissors");
+    let choice = prompt("What do you choose?");
     return choice;
 }
-
 let humanScore = 0;
 let computerScore = 0;
 
+for (let i = 0; i < 4; i++) {
+    const humanSelection = getHumanChoice();
+    console.log(`You chose ${humanSelection[0].toUpperCase() + humanSelection.substring(1).toLowerCase()}.`);
+    const computerSelection = getComputerChoice();
+    console.log(`Computer chose ${computerSelection}.`);
 
-//3. Compare the two choice variables to determine the winner
-
-function playRound(humanChoice, computerChoice) {
-  let firstLetter = humanChoice[0].toUpperCase();
-  let allOtherLetters = humanChoice.substr(1);
-  humanChoice = firstLetter + allOtherLetters;
-    
-    if (humanChoice === computerChoice) {
-        console.log("Draw! The round will be replayed.") //Round should be replayed here.
-    } if (humanChoice === "Rock" && computerChoice === "Paper") {
-        computerScore++;
-        console.log("You lose! Paper beats Rock.");
-    } if ((humanChoice === "Rock") && (computerChoice === "Scissor")) {
-        humanScore++;
-        console.log("You win! Rock beats Scissor.");
-    }else if ((humanChoice === "Paper") && (computerChoice === "Rock")) {
-        humanScore++; 
-        console.log("You win! Paper beats Rock.");
-    }else if ((humanChoice === "Paper") && (computerChoice === "Scissor")) {
-        computerScore++; 
-        console.log("You lose! Scissor beats Paper.");
-    }else if ((humanChoice === "Scissor") && (computerChoice === "Rock")) {
-        computerScore++; 
-        console.log("You lose! Rock beats Scissor.");
-    }else if ((humanChoice === "Scissor") && (computerChoice === "Paper")) {
-        humanScore++; 
-        console.log("You win! Scissor beats Paper.");
-    }
+    playGame(humanSelection, computerSelection);
+    console.log(humanScore, computerScore);
 }
 const humanSelection = getHumanChoice();
-console.log(`You chose ${humanSelection}.`);
+console.log(`You chose ${humanSelection[0].toUpperCase() + humanSelection.substring(1).toLowerCase()}.`);
 const computerSelection = getComputerChoice();
 console.log(`Computer chose ${computerSelection}.`);
+playGame(humanSelection, computerSelection);
+console.log(humanScore, computerScore);
+if (computerScore < humanScore) {
+    console.log("The human wins!");
+}else if (computerScore === humanScore) {
+    console.log("It's a draw.");
+} else {
+    console.log("Computer wins!")
+}
 
-playRound(humanSelection, computerSelection);
 
 //4. The winner gets a point stored into a counter
 //5. Run the game for 5 rounds and announce the overall winner. 
 
 
+function playGame(humanChoice, computerChoice) {
 
-// Step 1: Get Computer choice
 
-// Function getComputerChoice() when executed outputs "rock", "paper" or "scissors".
+
+    //3. Compare the two choice variables to determine the winner
+
+    function playRound(humanChoice, computerChoice) {
+        let firstLetter = humanChoice[0].toUpperCase();
+        let allOtherLetters = humanChoice.substring(1);
+        humanChoice = firstLetter + allOtherLetters;
+
+
+        if (humanChoice === computerChoice) {
+            console.log("Draw! The round will be replayed.") //Round should be replayed here.
+        } else if (humanChoice === "Rock" && computerChoice === "Paper") {
+            computerScore++;
+            console.log("You lose! Paper beats Rock.");
+        } else if ((humanChoice === "Rock") && (computerChoice === "Scissor")) {
+            humanScore++;
+            console.log("You win! Rock beats Scissor.");
+        } else if ((humanChoice === "Paper") && (computerChoice === "Rock")) {
+            humanScore++;
+            console.log("You win! Paper beats Rock.");
+        } else if ((humanChoice === "Paper") && (computerChoice === "Scissor")) {
+            computerScore++;
+            console.log("You lose! Scissor beats Paper.");
+        } else if ((humanChoice === "Scissor") && (computerChoice === "Rock")) {
+            computerScore++;
+            console.log("You lose! Rock beats Scissor.");
+        } else if ((humanChoice === "Scissor") && (computerChoice === "Paper")) {
+            humanScore++;
+            console.log("You win! Scissor beats Paper.");
+        }
+    }
+    playRound(humanChoice, computerChoice)
+}
+
