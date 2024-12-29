15672fcb436d66fdbfc259e879ed41506ef7ec3e
nike-dev <146763214+nike-dev@users.noreply.github.com>
2024-12-29 20:26:39 +0200

    Refactor playRound & create declareWinner function
    Created a declareWinner function as a helper to
    the playGame function. Refactored playRound to
    combine multiple if conditions with similar code
    blocks.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//Rock-Paper-Scissors Game
+
+//Global Score variables
+let humanScore = 0;
+let computerScore = 0;
+
+//Function to get Computer's choice
 function getComputerChoice() {
     let choice = Math.random() * 10;
     if (choice <= 3) {
@@ -11,55 +18,35 @@ function getComputerChoice() {
     }
 }
 
+//Function to get Human's choice
 function getHumanChoice() {
     let userInput = prompt("What do you choose?");
     let choice = userInput.trim();
     choice = choice[0].toUpperCase() + choice.substring(1).toLowerCase();
     return choice;
 }
+//function to take choices and execute each round
+function playRound(humanChoice, computerChoice) {
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
-
-        if (humanChoice === computerChoice) {
-            console.log("Draw! The round will be replayed.")
-        } else if (humanChoice === "Rock" && computerChoice === "Paper") {
-            computerScore++;
-            console.log("You lose! Paper beats Rock.");
-        } else if ((humanChoice === "Rock") && (computerChoice === "Scissors")) {
-            humanScore++;
-            console.log("You win! Rock beats Scissors.");
-        } else if ((humanChoice === "Paper") && (computerChoice === "Rock")) {
-            humanScore++;
-            console.log("You win! Paper beats Rock.");
-        } else if ((humanChoice === "Paper") && (computerChoice === "Scissors")) {
-            computerScore++;
-            console.log("You lose! Scissors beats Paper.");
-        } else if ((humanChoice === "Scissors") && (computerChoice === "Rock")) {
-            computerScore++;
-            console.log("You lose! Rock beats Scissors.");
-        } else if ((humanChoice === "Scissors") && (computerChoice === "Paper")) {
-            humanScore++;
-            console.log("You win! Scissors beats Paper.");
-        }
-        console.log(`Your score: ${humanScore} vs Computer score: ${computerScore}`);
+    if (humanChoice === computerChoice) {
+        console.log("Draw! The round will be replayed.")
+    } else if ((humanChoice === "Rock" && computerChoice === "Paper") ||
+        (humanChoice === "Paper" && computerChoice === "Scissors") ||
+        (humanChoice === "Scissors" && computerChoice === "Rock")) {
+        computerScore++;
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+    } else if ((humanChoice === "Rock" && computerChoice === "Scissors") ||
+        (humanChoice === "Paper" && computerChoice === "Rock") ||
+        (humanChoice === "Scissors" && computerChoice === "Paper")) {
+        humanScore++;
+        console.log("You win! ${humanChoice} beats ${computerChoice}.");
     }
+    console.log(`Your score: ${humanScore} vs Computer score: ${computerScore}`);
+    console.log("");
+}
 
-    for (let i = 0; i < 5; i++) {
-
-        const humanSelection = getHumanChoice();
-        console.log(`You chose ${humanSelection[0].toUpperCase() + humanSelection.substring(1).toLowerCase()}.`);
-        const computerSelection = getComputerChoice();
-        console.log(`Computer chose ${computerSelection}.`);
-
-        
-
-        playRound(humanSelection, computerSelection);
-    }
-
+//Prints final result to console
+function declareWinner() {
     if (humanScore === computerScore) {
         console.log("Final result: Draw!");
     } else if (humanScore > computerScore) {
@@ -69,7 +56,24 @@ function playGame() {
     }
 }
 
-playGame();
+//Main game function to play 5 rounds
+function playGame() {
+    //Loops the choices and rounds for upto 5
+    for (let i = 0; i < 5; i++) {
+
+        const humanSelection = getHumanChoice();
+        console.log(`You chose ${humanSelection[0].toUpperCase() + humanSelection.substring(1).toLowerCase()}.`);
+        const computerSelection = getComputerChoice();
+        console.log(`Computer chose ${computerSelection}.`);
+
+
+
+        playRound(humanSelection, computerSelection);
+    }
+    declareWinner();
+}
+
+playGame(); //Executes the game
 
 
 
