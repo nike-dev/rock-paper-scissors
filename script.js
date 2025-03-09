2a325b42acae6fdb4d6726de8399b84eb66bb114
nike-dev <146763214+nike-dev@users.noreply.github.com>
2025-03-09 17:09:02 +0200

    Add a condition to check only buttons fire events

--- script.js
+++ script.js
@@ -8,22 +8,26 @@ const btnContainer = document.createElement("div");
 const btnRock = document.createElement("button");
 btnRock.innerText = "Play Rock";
 btnRock.value = "Rock";
+btnRock.setAttribute("class", "btn");
 
 const btnPaper = document.createElement("button");
 btnPaper.innerText = "Play Paper";
 btnPaper.value = "Paper";
+btnPaper.setAttribute("class", "btn");
 
 
 const btnScissors = document.createElement("button");
 btnScissors.innerText = "Play Scissors";
 btnScissors.value = "Scissors";
+btnScissors.setAttribute("class", "btn");
+
+
 
 
 btnContainer.append(btnRock, btnPaper, btnScissors);
 container.append(btnContainer);
 
 function playRound(humanChoice, computerChoice) {
-
     if (humanChoice === computerChoice) {
         console.log("Draw! The round will be replayed.")
     } else if ((humanChoice === "Rock" && computerChoice === "Paper") ||
@@ -53,27 +57,30 @@ function declareWinner() {
 
 
 btnContainer.addEventListener("click", e => {
-    function getHumanChoice() {
-        const userInput = e.target.value;
-        let choice = userInput.trim();
-        choice = choice[0].toUpperCase() + choice.substring(1).toLowerCase();
-        return choice;
-    }
-    function getComputerChoice() {
-        const randomNum = Math.random() * 10;
-        let computerChoice;
+    const targetNodeName = e.target.nodeName.toLowerCase();
+    if (targetNodeName === "button") {
+        function getHumanChoice() {
+            const userInput = e.target.value;
+            let choice = userInput.trim();
+            choice = choice[0].toUpperCase() + choice.substring(1).toLowerCase();
+            return choice;
+        }
+        function getComputerChoice() {
+            const randomNum = Math.random() * 10;
+            let computerChoice;
 
-        computerChoice =
-            (randomNum <= 3)
-                ? "Rock"
-                : (randomNum <= 6)
-                    ? "Paper"
-                    : "Scissors";
-        return computerChoice;
+            computerChoice =
+                (randomNum <= 3)
+                    ? "Rock"
+                    : (randomNum <= 6)
+                        ? "Paper"
+                        : "Scissors";
+            return computerChoice;
+        }
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
     }
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
 });
 
 
